6dd2cc4c3e65b2d2f2ad94009148714d0bb5c0a9
Tim Engbrocks <tim.engbrocks@student.kit.edu>
2023-11-03 09:01:57 +0100

    Run/Debug scripts: bail if cargo build fails

--- debug.sh
+++ debug.sh
@@ -2,4 +2,10 @@
 
 cargo build
 
+EXIT=$(echo $?)
+
+if [ $EXIT -ne 0 ]; then
+	exit $EXIT
+fi
+
 gdb --args ./target/debug/dione -c Main.class -j ./jdk
--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 cargo build
 
+EXIT=$(echo $?)
+
+if [ $EXIT -ne 0 ]; then
+	exit $EXIT
+fi
+
 RUST_BACKTRACE=1 ./target/debug/dione -c Main -j ./jdk
