9629ff8c78abffdb3a8a707264cbf34635fcdecc
Tim Engbrocks <engbrocks.tim@gmail.com>
2025-03-24 21:38:22 +0100

    Move tests to separate folder

---
+++ class_file_parser/tests/tests.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use class_file_parser::class_file::class_file_parser;
+
+const CLASS_FILE_PATH: &str = "./Main.class";
+
+#[test]
+fn parse_class_file() {
+	let class_file_raw = match fs::read(CLASS_FILE_PATH) {
+		Ok(v) => v,
+		Err(e) => panic!("Failed to read class file. Got: '{e}'"),
+	};
+
+	let (rest, _class_file) = match class_file_parser::<()>(&class_file_raw[..]) {
+		Ok(v) => v,
+		Err(e) => panic!("Failed to parse class file. Got: '{e}'"),
+	};
+	if !rest.is_empty() {
+		panic!("Failed to parse complete class file. Have {} bytes remaining!", rest.len());
+	}
+}
